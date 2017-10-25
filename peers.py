58dabdf623fc15f37daeab97ec9b96fe8cbd7302
Peter Pentchev <pp@storpool.com>
2017-10-25 05:41:03 -0400

    Code style: docstrings.

--- peers.py
+++ peers.py
@@ -1,3 +1,7 @@
+"""
+A Juju charm interface that helps the `storpool-block` charm keep track of
+which units have been installed and configured.
+"""
 import json
 
 from charms import reactive
@@ -7,14 +11,24 @@ from spcharms import utils as sputils
 
 
 def rdebug(s):
+    """
+    Pass the diagnostic message string `s` to the central diagnostic logger.
+    """
     sputils.rdebug(s, prefix='storpool-service')
 
 
 class StorPoolServicePeer(reactive.RelationBase):
+    """
+    The Juju interface class that keeps track of presence data received from
+    the other `storpool-block` units.
+    """
     scope = reactive.scopes.UNIT
 
     @reactive.hook('{peers:storpool-service}-relation-{joined,changed}')
     def peer_changed(self):
+        """
+        Handle data received from the other units.
+        """
         rdebug('relation-joined/changed invoked')
         try:
             conv = self.conversation()
@@ -53,6 +67,10 @@ class StorPoolServicePeer(reactive.RelationBase):
 
     @reactive.hook('{peers:storpool-service}-relation-departed')
     def peer_departed(self):
+        """
+        Handle the relation break-down case (all other units departed or
+        this unit being shutdown).
+        """
         rdebug('relation-departed')
         try:
             service_hook.handle(self, False, None, rdebug=rdebug)
