15e24020a7524fa51dcbad88a006d349e1dcfa9b
Peter Pentchev <pp@storpool.com>
2018-03-27 17:38:49 +0300

    Simplify the tests a lot with the reworked hooks.

--- unit_tests/lib/spcharms/__init__.py
+++ unit_tests/lib/spcharms/__init__.py
@@ -2,52 +2,8 @@
 
 import mock
 
-
-class SPServiceHook(object):
-    """
-    Trivially simulate the service_hook utility helper with
-    none of its actual functionality, just record all the calls
-    to the handle() method.
-    """
-    def __init__(self):
-        """
-        Initialize an object with no recorded calls.
-        """
-        self.data = []
-        self.changed = False
-
-    def handle(self, data, rdebug):
-        """
-        Record the arguments passed to the method.
-        """
-        self.data.append((data, rdebug))
-        return self.changed
-
-    def get_data(self):
-        """
-        Return the recorded data.
-        """
-        return list(self.data)
-
-    def clear_data(self):
-        """
-        Clear the recorded data for a new test.
-        """
-        self.data = []
-
-    def get_state(self):
-        """
-        Mock the service_hook's object state dictionary and its "changed" flag.
-        """
-        return ('me!', self.changed)
-
-    def set_changed(self, changed):
-        """
-        Set the "changed" flag for the later get_state() invocations.
-        """
-        self.changed = changed
-
-
 utils = mock.Mock()
+utils.rdebug = mock.Mock()
 
-service_hook = SPServiceHook()
+service_hook = mock.Mock()
+service_hook.handle_remote_presence = mock.Mock()
