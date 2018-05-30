00f9cb8109f5821b4a7dd6aaa9c9b2da86d905c9
Peter Pentchev <pp@storpool.com>
2018-05-30 14:08:12 +0300

    Just notify the charm, leave all the handling to it.

--- peers.py
+++ peers.py
@@ -2,11 +2,8 @@
 A Juju charm interface that helps the `storpool-block` charm keep track of
 which units have been installed and configured.
 """
-import json
-
 from charms import reactive
 
-from spcharms import service_hook
 from spcharms import utils as sputils
 
 
@@ -27,53 +24,17 @@ class StorPoolServicePeer(reactive.RelationBase):
     @reactive.hook('{peers:storpool-service}-relation-{joined,changed}')
     def peer_changed(self):
         """
-        Handle data received from the other units.
+        Let the main charm handle data received from the other units.
         """
         rdebug('relation-joined/changed invoked')
-        try:
-            conv = self.conversation()
-            data = conv.get_remote('storpool_service')
-            if data is None:
-                rdebug('- no service data yet, sending ours then')
-                (state, _) = service_hook.get_state()
-                rdebug('- just making sure we have "-local" in '
-                       'the state array: {have}'
-                       .format(have='-local' in state))
-                conv.set_remote('storpool_service',
-                                json.dumps(state['-local']))
-                rdebug('- done with this hook, it seems')
-                return
-            data = json.loads(data)
-            if not isinstance(data, dict):
-                rdebug('- hmmm, deserialized the service data, but not '
-                       'a dictionary - "{t}" instead'
-                       .format(t=type(data).__name__))
-                return
+        self.set_state('{relation_name}.present')
+        self.set_state('{relation_name}.notify')
 
-            rdebug('- we got some data from the other side: {data}'
-                   .format(data=data))
-            changed = service_hook.handle(self, True, data, rdebug=rdebug)
-            if changed:
-                rdebug('- looks like something changed when we received '
-                       'the data, sending ours across')
-                (state, _) = service_hook.get_state()
-                rdebug('- got full state: {state}'.format(state=state))
-                conv.set_remote('storpool_service',
-                                json.dumps(state['-local']))
-                rdebug('- looks fine')
-        except Exception as e:
-            rdebug('could not complete the relation '
-                   'joined/changed actions: {e}'.format(e=e))
-
-    @reactive.hook('{peers:storpool-service}-relation-departed')
-    def peer_departed(self):
+    @reactive.hook('{peers:storpool-service}-relation-{departed,broken}')
+    def peer_gone(self):
         """
-        Handle the relation break-down case (all other units departed or
-        this unit being shutdown).
+        Let the main charm know that our peers have left the building.
         """
-        rdebug('relation-departed')
-        try:
-            service_hook.handle(self, False, None, rdebug=rdebug)
-        except Exception as e:
-            rdebug('could not complete the relation departed actions: {e}'
-                   .format(e=e))
+        rdebug('relation-departed/broken invoked')
+        self.remove_state('{relation_name}.present')
+        self.remove_state('{relation_name}.notify')
--- unit_tests/lib/spcharms/__init__.py
+++ unit_tests/lib/spcharms/__init__.py
@@ -2,52 +2,5 @@
 
 import mock
 
-
-class SPServiceHook(object):
-    """
-    Trivially simulate the service_hook utility helper with
-    none of its actual functionality, just record all the calls
-    to the handle() method.
-    """
-    def __init__(self):
-        """
-        Initialize an object with no recorded calls.
-        """
-        self.data = []
-        self.changed = False
-
-    def handle(self, obj, attaching, data, rdebug):
-        """
-        Record the arguments passed to the method.
-        """
-        self.data.append((obj, attaching, data, rdebug))
-        return self.changed
-
-    def get_data(self):
-        """
-        Return the recorded data.
-        """
-        return list(self.data)
-
-    def clear_data(self):
-        """
-        Clear the recorded data for a new test.
-        """
-        self.data = []
-
-    def get_state(self):
-        """
-        Mock the service_hook's object state dictionary and its "changed" flag.
-        """
-        return ({'-local': 'me!'}, self.changed)
-
-    def set_changed(self, changed):
-        """
-        Set the "changed" flag for the later get_state() invocations.
-        """
-        self.changed = changed
-
-
 utils = mock.Mock()
-
-service_hook = SPServiceHook()
+utils.rdebug = mock.Mock()
